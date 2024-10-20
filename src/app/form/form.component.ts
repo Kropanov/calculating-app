167ba746dc3515a352a61dd20219561c412f82a2
Maksim Kropanov <kropanov2012@gmail.com>
2024-10-20 15:12:56 +0300

    feat: complete logic of the form container

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { PaginatorModule } from 'primeng/paginator';
+import { Button } from 'primeng/button';
 
 @Component({
-  selector: 'app-form',
-  standalone: true,
-  imports: [],
-  templateUrl: './form.component.html',
-  styleUrl: './form.component.scss'
+    selector: 'app-form',
+    standalone: true,
+    imports: [PaginatorModule, Button],
+    templateUrl: './form.component.html',
+    styleUrl: './form.component.scss',
 })
 export class FormComponent {
-
+    enteredValue: number = 0;
 }
